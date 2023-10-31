705fa18a0365807f6673a513f395930109b6e89a
Philipp <philippphan@gmail.com>
2023-11-01 00:24:00 +0100

    :ambulance: Controller added

--- src/main/java/controller/CompetitionController.java
+++ src/main/java/controller/CompetitionController.java
@@ -1,9 +1,34 @@
 package controller;
 
+import entities.Competition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import repositories.CompetitionRepository;
 
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "competition")
 public class CompetitionController {
 
+    @Autowired
     private CompetitionRepository competitionRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addCompetition
+            (@RequestParam String name) {
+
+        Competition competition = new Competition();
+        competition.setCompetitionName(name);
+
+        return "Done";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<Competition> getAllTeams(){
+        // This returns a JSON or XML with the teams
+        return competitionRepository.findAll();
+    }
 
 }
--- src/main/java/controller/MatchController.java
+++ src/main/java/controller/MatchController.java
@@ -1,9 +1,39 @@
 package controller;
 
+import entities.Competition;
+import entities.Match;
+import entities.Scoreline;
+import entities.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import repositories.MatchRepository;
 
+@Controller
+@RequestMapping(path = "match")
 public class MatchController {
 
+    @Autowired
     private MatchRepository matchRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addMatch(@RequestParam Team homeTeam,
+                                         @RequestParam Team awayTeam,
+                                         @RequestParam Scoreline scoreline,
+                                         @RequestParam Competition competition) {
+
+        Match match = new Match();
+        match.setHomeTeam(homeTeam);
+        match.setAwayTeam(awayTeam);
+        match.setScoreLine(scoreline);
+        match.setCompetetion(competition);
+
+        return "Done";
+    }
+
+    @GetMapping(path = "all")
+    public @ResponseBody Iterable<Match> getAllMatches(){
+        return matchRepository.findAll();
+    }
 
 }
